2abc49b9161f388549836bed6daf608e77f8b1a7
dedestem <david@davidnet.net>
2025-05-25 17:34:15 +0200

    DEV BUILD -- Needing font

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { IconButton } from "@davidnet/svelte-ui";
+</script>
+
+<nav>
+	<IconButton appearance="subtle" icon="left_panel_close"></IconButton>
+	<img class="logo" src="/Assets/Images/Logos/svelte-ui-cropped.png" alt="" aria-hidden="true" />
+</nav>
+
+<style lang="scss">
+	nav {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		align-content: flex-start;
+		padding: 4px;
+		background-color: #1f1f21;
+		height: 40px;
+	}
+
+	.logo {
+		height: 40px;
+		width: 40px;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,45 +1,6 @@
 <script lang="ts">
+	import Nav from "$lib/components/Nav.svelte";
 	import { Button } from "@davidnet/svelte-ui";
 </script>
 
-<h1 style="font-family: Open Sans">Component Showcase</h1>
-<h2 style="font-family: Open Sans">Button appearance</h2>
-<p style="font-family: Open Sans; font-weight: bold;">
-	import &#123; Button &#125; from '@davidnet/svelte-ui';
-</p>
-<span style="padding: 10px;"></span>
-<p style="font-family: Open Sans; font-weight: bold;">
-	&lt;Button appearance="subtle"&gt;Subtle Button &lt;Button&gt;
-</p>
-<Button appearance="subtle">Subtle Button</Button>
-<span style="padding: 10px;"></span>
-<p style="font-family: Open Sans; font-weight: bold;">
-	&lt;Button appearance="default"&gt;Subtle Button &lt;Button&gt;
-</p>
-<Button appearance="default">Default Button</Button>
-<span style="padding: 10px;"></span>
-<p style="font-family: Open Sans; font-weight: bold;">
-	&lt;Button appearance="primary"&gt;Subtle Button &lt;Button&gt;
-</p>
-<Button appearance="primary">Primary Button</Button>
-<span style="padding: 10px;"></span>
-<p style="font-family: Open Sans; font-weight: bold;">
-	&lt;Button appearance="warning"&gt;Subtle Button &lt;Button&gt;
-</p>
-<Button appearance="warning">Warning Button</Button>
-<span style="padding: 10px;"></span>
-<p style="font-family: Open Sans; font-weight: bold;">
-	&lt;Button appearance="danger"&gt;Subtle Button &lt;Button&gt;
-</p>
-<Button appearance="danger">Danger Button</Button>
-
-<span style="padding: 20px;"></span>
-<p style="font-family: Open Sans; font-weight: bold;">
-	&lt;Button appearance="default" iconBefore="delete" &gt;Subtle Button &lt;Button&gt;
-</p>
-<Button appearance="default" iconBefore="delete">IconBefore Button</Button>
-<span style="padding: 10px;"></span>
-<p style="font-family: Open Sans; font-weight: bold;">
-	&lt;Button appearance="default" iconAfter="delete" &gt;Subtle Button &lt;Button&gt;
-</p>
-<Button appearance="default" iconAfter="delete">IconAfter Button</Button>
+<Nav></Nav>
