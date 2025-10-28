5b9bc8b1b3fae8467c82838096da7695c84550d7
dedestem <david@davidnet.net>
2025-10-28 20:55:53 +0100

    Add meta

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
 	import { ThemeProvider, Toaster, ConnectivityCheck } from "@davidnet/svelte-ui";
+
+	const initloader = document.getElementById("initloader");
+	if (initloader) initloader.remove();
 </script>
 
 <ThemeProvider />
